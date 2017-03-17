c6091b8b9a477a5d82e0565b4eebfd78045cbc52
dean <hhuang@goodwatercap.com>
2017-03-17 09:52:09 -0700

    Import my MineSweeper app from Windows.

---
+++ MineSweeper/MineSweeper.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Game
+{
+    partial class MineSweeperForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start = new System.Windows.Forms.Button();
+            this.gameSize = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.newGame = new System.Windows.Forms.Button();
+            this.Mine_label = new System.Windows.Forms.Label();
+            this.numberOfMines = new System.Windows.Forms.TextBox();
+            this.timeText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(245, 41);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 3;
+            this.start.Text = "START";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // gameSize
+            // 
+            this.gameSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.gameSize.FormattingEnabled = true;
+            this.gameSize.Location = new System.Drawing.Point(48, 9);
+            this.gameSize.Name = "gameSize";
+            this.gameSize.Size = new System.Drawing.Size(75, 21);
+            this.gameSize.TabIndex = 5;
+            this.gameSize.SelectedIndexChanged += new System.EventHandler(this.gameSize_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Size:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Total Time:";
+            // 
+            // newGame
+            // 
+            this.newGame.Location = new System.Drawing.Point(131, 374);
+            this.newGame.Name = "newGame";
+            this.newGame.Size = new System.Drawing.Size(75, 32);
+            this.newGame.TabIndex = 8;
+            this.newGame.Text = "New Game";
+            this.newGame.UseVisualStyleBackColor = true;
+            this.newGame.Click += new System.EventHandler(this.newGame_Click_1);
+            // 
+            // Mine_label
+            // 
+            this.Mine_label.AutoSize = true;
+            this.Mine_label.Location = new System.Drawing.Point(202, 13);
+            this.Mine_label.Name = "Mine_label";
+            this.Mine_label.Size = new System.Drawing.Size(35, 13);
+            this.Mine_label.TabIndex = 11;
+            this.Mine_label.Text = "Mines";
+            // 
+            // numberOfMines
+            // 
+            this.numberOfMines.Location = new System.Drawing.Point(243, 10);
+            this.numberOfMines.Name = "numberOfMines";
+            this.numberOfMines.Size = new System.Drawing.Size(77, 20);
+            this.numberOfMines.TabIndex = 12;
+            // 
+            // timeText
+            // 
+            this.timeText.AutoSize = true;
+            this.timeText.Location = new System.Drawing.Point(90, 50);
+            this.timeText.Name = "timeText";
+            this.timeText.Size = new System.Drawing.Size(0, 13);
+            this.timeText.TabIndex = 13;
+            // 
+            // GameUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 418);
+            this.Controls.Add(this.timeText);
+            this.Controls.Add(this.numberOfMines);
+            this.Controls.Add(this.Mine_label);
+            this.Controls.Add(this.newGame);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gameSize);
+            this.Controls.Add(this.start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "GameUI";
+            this.Text = "MineSweeper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.ComboBox gameSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button newGame;
+        private System.Windows.Forms.Label Mine_label;
+        private System.Windows.Forms.TextBox numberOfMines;
+        private System.Windows.Forms.Label timeText;
+    }
+}
+
---
+++ MineSweeper/MineSweeper.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class MineSweeperForm : Form
+    {
+        //private static Timer timer = new Timer();
+        private static Button[,] buttonGrid;
+        private static int[,] map;
+        private static int mines = 0;
+        private static int currentGameSize = 0;
+        private static int _ticks = 0;
+
+        //initialize the UI
+        public MineSweeperForm()
+        {
+            InitializeComponent();
+            generateGameSizeComboBox();
+        }
+
+        //initilize game size list
+        private void generateGameSizeComboBox()
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                gameSize.Items.Add(i + " * " + i);
+            }
+        }
+
+        //start the game
+        private void start_Click(object sender, EventArgs e)
+        {
+            //check number of mines and selected size, if valid start the game
+            if (isValueValid())
+            {
+                clearPreviousGame();
+                generateGame(currentGameSize, mines);
+                //print the map in console
+                for (int i = 0; i < map.GetLength(0); ++i)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    for (int j = 0; j < map.GetLength(1); ++j)
+                    {
+                        sb.Append(map[j, i] + " ");
+                        //Trace.Write(map[j, i] + " ");
+                    }
+                    Trace.WriteLine(sb.ToString());
+                }
+                //start timer when game is generated
+                timer1.Start();
+            }
+        }
+
+        //change the current selected size
+        private void gameSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currentGameSize = gameSize.SelectedIndex + 1;
+        }
+
+        //check the insert value and select option is valid
+        private bool isValueValid()
+        {
+            int tmp = 0;
+            try
+            {
+                if (currentGameSize <= 0)
+                {
+                    MessageBox.Show("please select size of your game!");
+                    return false;
+                }
+                tmp = Convert.ToInt32(numberOfMines.Text);
+                if (tmp <= 0 || tmp > Math.Pow(currentGameSize, 2))
+                {
+                    MessageBox.Show(String.Format("please insert a valid number of mines! {0} is less than 1 or greater than the gamesize {1}", tmp, Math.Pow(currentGameSize, 2)));
+                    return false;
+                }
+                else
+                {
+                    mines = tmp;
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(String.Format("please insert a valid integer!"));
+                Program.logIt(ex.ToString());
+                return false;
+            }
+        }
+
+        //clear previous game
+        private void clearPreviousGame()
+        {
+            try
+            {
+                _ticks = 0;
+                if (buttonGrid != null | buttonGrid.Length != 0)
+                {
+                    for (int i = 0; i < buttonGrid.GetLength(0); ++i)
+                    {
+                        for (int j = 0; j < buttonGrid.GetLength(1); ++j)
+                        {
+                            if (Controls.Contains(buttonGrid[i, j]))
+                            {
+                                Controls.Remove(buttonGrid[i, j]);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Program.logIt(e.ToString());
+            }
+        }
+
+        //generate the game grid panel here
+        private void generateGame(int size, int mines)
+        {
+            try
+            {
+                Program.logIt(String.Format("size:{0}  mines:{1}", size, mines));
+
+                //create buttons
+                buttonGrid = new Button[size, size];
+                //start point is 20, 76
+                int buttonSize = 300 / size;
+                for (int i = 0; i < size; ++i)
+                {
+                    for (int j = 0; j < size; ++j)
+                    {
+                        buttonGrid[i, j] = new Button();
+                        buttonGrid[i, j].Name = i + "," + j;
+                        buttonGrid[i, j].Size = new System.Drawing.Size(buttonSize, buttonSize);//because the panel size is 300*300
+                        buttonGrid[i, j].Location = new System.Drawing.Point(20 + (i * buttonSize), 76 + (j * buttonSize));//keep adding from the start point
+                        Controls.AddRange(new System.Windows.Forms.Control[] { buttonGrid[i, j], });
+                        buttonGrid[i, j].Click += new System.EventHandler(gameButtonClick);
+                    }
+                }
+
+                //get randomized mines
+                map = new int[size, size];
+                int randomMine = mines;
+                Random random = new Random();
+                while (randomMine > 0)
+                {
+                    int i = 0;
+                    int j = 0;
+                    i = random.Next(0, size);
+                    //Random rndomJ = new Random();
+                    j = random.Next(0, size);
+
+                    Console.Write(string.Format("{0},{1} ", i, j));
+                    if (map[i, j] != 1)
+                    {
+                        map[i, j] = 1;
+                        randomMine--;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                Console.WriteLine();
+            }
+            catch (Exception e)
+            {
+                Program.logIt(e.ToString());
+            }
+        }
+
+        //hit each game button
+        void gameButtonClick(object sender, EventArgs e)
+        {
+            Button bttnClick = sender as Button;
+
+            string[] split = bttnClick.Name.Split(new Char[] { ',' });
+
+            int i = System.Convert.ToInt32(split[0]);
+            int j = System.Convert.ToInt32(split[1]);
+
+            Program.logIt("this position value = " + map[i, j].ToString());
+
+            //change the font size based on the button size
+            int fontSize = Convert.ToInt16(buttonGrid[0, 0].Height);
+            if (fontSize > 0 && fontSize < 100)
+                fontSize = 15;
+            else if (fontSize >= 100 && fontSize <= 200)
+                fontSize = 25;
+            else
+                fontSize = 30;
+
+            if (map[i, j] != 1)
+            {
+                Program.logIt(string.Format("clicked button[{0},{1}]", i, j));
+                //Game Over!
+                bool isMineNearBy = false;
+                int countMine = 0;
+                //check surrounding 8 cells and see how many mines are there
+                if ((i - 1 >= 0 && j - 1 >= 0) && map[i - 1, j - 1] == 1)
+                {
+                    isMineNearBy = true;
+                    countMine++;
+                }
+
+                if (i - 1 >= 0 && map[i - 1, j] == 1)
+                {
+                    isMineNearBy = true;
+                    countMine++;
+                }
+
+                if (j - 1 >= 0 && map[i, j - 1] == 1)
+                {
+                    isMineNearBy = true;
+                    countMine++;
+                }
+
+                if ((i + 1 < buttonGrid.GetLength(0) && j + 1 < buttonGrid.GetLength(1)) && map[i + 1, j + 1] == 1)
+                {
+                    isMineNearBy = true;
+                    countMine++;
+                }
+
+                if (i + 1 < buttonGrid.GetLength(0) && map[i + 1, j] == 1)
+                {
+                    isMineNearBy = true;
+                    countMine++;
+                }
+
+                if (j + 1 < buttonGrid.GetLength(1) && map[i, j + 1] == 1)
+                {
+                    isMineNearBy = true;
+                    countMine++;
+                }
+
+                if (i + 1 < buttonGrid.GetLength(0) && j - 1 >= 0 && map[i + 1, j - 1] == 1)
+                {
+                    isMineNearBy = true;
+                    countMine++;
+                }
+
+                if (i - 1 >= 0 && j + 1 < buttonGrid.GetLength(1) && map[i - 1, j + 1] == 1)
+                {
+                    isMineNearBy = true;
+                    countMine++;
+                }
+
+                //caution move
+                if (isMineNearBy)
+                {
+                    Program.logIt("mine near this position");
+                    buttonGrid[i, j].Text = countMine.ToString();
+                    buttonGrid[i, j].Font = new Font("Microsoft Sans Serif", (float)fontSize, buttonGrid[i, j].Font.Style, buttonGrid[i, j].Font.Unit);
+                    buttonGrid[i, j].Enabled = false;
+                }
+                //safe move
+                else
+                {
+                    Program.logIt("no mine near this position");
+                    buttonGrid[i, j].Text = "";
+                    buttonGrid[i, j].Enabled = false;
+                }
+            }
+            else
+            {
+                //GAME END HERE AND SHOW ALL MINES AND DESABLE ALL CELLS
+                Program.logIt(string.Format("clicked button[{0},{1}]", i, j));
+                Program.logIt("Gameover");
+                timer1.Stop();//timer stops here
+                buttonGrid[i, j].Text = "*";
+                buttonGrid[i, j].Font = new Font("Microsoft Sans Serif", (float)fontSize * 2, buttonGrid[i, j].Font.Style, buttonGrid[i, j].Font.Unit);
+                buttonGrid[i, j].Enabled = false;
+                buttonGrid[i, j].BackColor = Color.Red;
+                for (int a = 0; a < buttonGrid.GetLength(0); ++a)
+                {
+                    for (int b = 0; b < buttonGrid.GetLength(1); ++b)
+                    {
+                        if (map[a, b] == 1)
+                        {
+                            buttonGrid[a, b].Text = "*";
+                            buttonGrid[a, b].Font = new Font("Microsoft Sans Serif", (float)fontSize * 2, buttonGrid[a, b].Font.Style, buttonGrid[i, j].Font.Unit);
+                            buttonGrid[a, b].Enabled = false;
+                            continue;
+                        }
+                        buttonGrid[a, b].Enabled = false;
+                    }
+                }
+                MessageBox.Show("GameOver!");
+            }
+        }
+
+        //clear game
+        private void newGame_Click_1(object sender, EventArgs e)
+        {
+            clearPreviousGame();
+        }
+
+        //time tick update
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _ticks++;
+            timeText.Text = _ticks.ToString();
+        }
+
+    }
+}
+
